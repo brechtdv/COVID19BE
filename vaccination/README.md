d977a63244e2f0f9a5f9d0658347bfcf5daa56c5
Brecht <brechtdv@gmail.com>
2022-01-09 13:40:05 +0100

    add booster coverage

--- vaccination/README.md
+++ vaccination/README.md
@@ -9,6 +9,10 @@
 
 ![](covid19be-vaccination.png)
 
+### COVID-19 14-day cumulative incidence vs booster rate by municipality
+
+![](covid19be-vaccination-booster.png)
+
 ### Correlation cumulative incidence vs vaccination rate
 
 |                | estimate.rho | p.value |
@@ -25,6 +29,21 @@
 | VlaamsBrabant  |        0.799 |   0.000 |
 | WestVlaanderen |      \-0.141 |   0.267 |
 | Belgium        |        0.168 |   0.000 |
+
+|                | estimate.rho | p.value |
+| :------------- | -----------: | ------: |
+| Antwerpen      |        0.284 |   0.018 |
+| BrabantWallon  |        0.101 |   0.614 |
+| Brussels       |      \-0.105 |   0.667 |
+| Hainaut        |        0.011 |   0.931 |
+| Liège          |      \-0.155 |   0.160 |
+| Limburg        |        0.575 |   0.000 |
+| Luxembourg     |        0.174 |   0.257 |
+| Namur          |        0.323 |   0.048 |
+| OostVlaanderen |        0.290 |   0.025 |
+| VlaamsBrabant  |        0.626 |   0.000 |
+| WestVlaanderen |        0.124 |   0.329 |
+| Belgium        |        0.275 |   0.000 |
 
 ### Data source
 
--- vaccination/covid19be-vaccination.R
+++ vaccination/covid19be-vaccination.R
@@ -1,5 +1,5 @@
 ### COVID19BE // VACCINATION
-### 18/11/2021
+### 09/01/2022
 
 ## required packages
 library(ggplot2)
@@ -37,13 +37,20 @@ dta_vac <-
 read.csv(
   encoding = "UTF-8",
   "https://epistat.sciensano.be/data/COVID19BE_VACC_MUNI_CUM.csv")
-dta_vac_agg <-
+dta_vac_agg_bc <-
 aggregate(
   as.numeric(CUMUL) ~ NIS5,
   subset(dta_vac,
     YEAR_WEEK == max(dta_vac$YEAR_WEEK) & DOSE %in% c("B", "C")),
   sum, na.rm = TRUE)
-names(dta_vac_agg) <- c("NIS5", "VAC")
+names(dta_vac_agg_bc) <- c("NIS5", "VAC")
+dta_vac_agg_e <-
+aggregate(
+  as.numeric(CUMUL) ~ NIS5,
+  subset(dta_vac,
+    YEAR_WEEK == max(dta_vac$YEAR_WEEK) & DOSE == "E"),
+  sum, na.rm = TRUE)
+names(dta_vac_agg_e) <- c("NIS5", "VAC")
 
 ## harmonize province names dta1 if needed
 if (!("PROVINCE" %in% names(dta_inc1)))
@@ -56,47 +63,62 @@ data.frame(
   MUNI = dta_inc1$TX_DESCR_NL,
   PROV = dta_inc1$PROVINCE,
   N14 = as.numeric(dta_inc3$CASES) - as.numeric(dta_inc1$CASES),
-  VAC = c(dta_vac_agg$VAC, NA))
+  VAC_BC = c(dta_vac_agg_bc$VAC, NA),
+  VAC_E = c(dta_vac_agg_e$VAC, NA))
 
 ## calculate incidence per 100k
 load("POPNIS5.Rdata")  # objects 'POP' and 'POPNIS5'
 dta <- merge(dta, POPNIS5)
 dta$INC14 <- 1e5 * dta$N14 / dta$MS_POPULATION
-dta$NON_VAC_RT <- 1 - dta$VAC / dta$MS_POPULATION
+dta$NON_VAC_BC_RT <- 1 - dta$VAC_BC / dta$MS_POPULATION
+dta$NON_VAC_E_RT <- 1 - dta$VAC_E / dta$MS_POPULATION
 
 ## identify top municipalities for labelling
 id1 <- tail(dta[order(dta$INC14), "MUNI"], 5)
-id2 <- tail(dta[order(dta$NON_VAC_RT), "MUNI"], 5)
-(id <- unique(c(id1, id2)))
+id2_bc <- tail(dta[order(dta$NON_VAC_BC_RT), "MUNI"], 5)
+(id_bc <- unique(c(id1, id2_bc)))
+id2_e <- tail(dta[order(dta$NON_VAC_E_RT), "MUNI"], 5)
+(id_e <- unique(c(id1, id2_e)))
 
 ## spearman correlation coefficient // BE
-cor <- with(dta, cor.test(INC14, NON_VAC_RT, method = "spearman"))
+cor_bc <- with(dta, cor.test(INC14, NON_VAC_BC_RT, method = "spearman"))
+cor_e <- with(dta, cor.test(INC14, NON_VAC_E_RT, method = "spearman"))
 
 ## spearman correlation coefficient // PROV
-cor_prov <-
+cor_prov_bc <-
 tapply(seq(nrow(dta)), dta$PROV,
   function(x)
-    with(dta[x,], cor.test(INC14, NON_VAC_RT, method = "spearman")))
-cor_prov <-
+    with(dta[x,], cor.test(INC14, NON_VAC_BC_RT, method = "spearman")))
+cor_prov_bc <-
 rbind(
   as.data.frame(
-    t(sapply(cor_prov, function(x) unlist(x[c("estimate", "p.value")])))),
-  Belgium = data.frame(estimate.rho = cor$estimate,
-                       p.value = cor$p.value))
+    t(sapply(cor_prov_bc, function(x) unlist(x[c("estimate", "p.value")])))),
+  Belgium = data.frame(estimate.rho = cor_bc$estimate,
+                       p.value = cor_bc$p.value))
+cor_prov_e <-
+tapply(seq(nrow(dta)), dta$PROV,
+  function(x)
+    with(dta[x,], cor.test(INC14, NON_VAC_E_RT, method = "spearman")))
+cor_prov_e <-
+rbind(
+  as.data.frame(
+    t(sapply(cor_prov_e, function(x) unlist(x[c("estimate", "p.value")])))),
+  Belgium = data.frame(estimate.rho = cor_e$estimate,
+                       p.value = cor_e$p.value))
 
 ##
 ## PLOT
 ##
 
 png("covid19be-vaccination.png", 8, 6, units = "in", res = 300)
-ggplot(dta, aes(x = INC14, y = NON_VAC_RT, size = N14, color = PROV)) +
+ggplot(dta, aes(x = INC14, y = NON_VAC_BC_RT, size = N14, color = PROV)) +
   geom_smooth(
-    aes(x = INC14, y = NON_VAC_RT),
+    aes(x = INC14, y = NON_VAC_BC_RT),
     inherit.aes = FALSE,
     method = lm) +
   geom_point() +
   geom_text_repel(
-    data = subset(dta, MUNI %in% id),
+    data = subset(dta, MUNI %in% id_bc),
     aes(label = MUNI),
     show.legend = FALSE) +
   annotate(
@@ -129,7 +151,55 @@ ggplot(dta, aes(x = INC14, y = NON_VAC_RT, size = N14, color = PROV)) +
               format(last_day - 1, "%d/%m")),
     subtitle =
       sprintf("Spearman correlation coefficient: %s",
-              formatC(cor$estimate, format = "f", digits = 3)),
+              formatC(cor_bc$estimate, format = "f", digits = 3)),
+    caption =
+      paste("Note: data are cases by date of reporting, not by date of diagnosis",
+            "Source code: https://github.com/brechtdv/COVID19BE",
+            sep = "\n"))
+dev.off()
+
+png("covid19be-vaccination-booster.png", 8, 6, units = "in", res = 300)
+ggplot(dta, aes(x = INC14, y = NON_VAC_E_RT, size = N14, color = PROV)) +
+  geom_smooth(
+    aes(x = INC14, y = NON_VAC_E_RT),
+    inherit.aes = FALSE,
+    method = lm) +
+  geom_point() +
+  geom_text_repel(
+    data = subset(dta, MUNI %in% id_e),
+    aes(label = MUNI),
+    show.legend = FALSE) +
+  annotate(
+    "text",
+    x = c(-Inf, Inf, -Inf, Inf),
+    y = c(-Inf, Inf, -Inf, Inf),
+    label = c("\u2190 Low percentage non-boostered", "High percentage non-boostered\u2192",
+              "\u2190 Low incidence", "High incidence \u2192"),
+    angle = c(90, 90, 0, 0),
+    hjust = c(-0.1, 1.1, -0.1, 1.1),
+    vjust = c(1.5, -0.5, -0.5, 1.5),
+    size = 3.5,
+    color = "grey") +
+  theme_bw() +
+  theme(plot.caption.position =  "plot") +
+  scale_x_continuous(
+    expand = expansion(mult = 0.075)) +
+  scale_y_continuous(
+    expand = expansion(mult = 0.075),
+    labels = scales::percent) +
+  labs(
+    x = paste("Cumulative 14-day incidence per 100,000",
+              sep = "\n"),
+    y = "Percentage not boostered",
+    size = "Total nr of cases",
+    color = "Province",
+    title =
+      sprintf("COVID19BE incidence and booster coverage by municipality, %s to %s",
+              format(last_day - 15, "%d/%m"),
+              format(last_day - 1, "%d/%m")),
+    subtitle =
+      sprintf("Spearman correlation coefficient: %s",
+              formatC(cor_e$estimate, format = "f", digits = 3)),
     caption =
       paste("Note: data are cases by date of reporting, not by date of diagnosis",
             "Source code: https://github.com/brechtdv/COVID19BE",
